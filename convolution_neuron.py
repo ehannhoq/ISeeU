9f79c0d8083ae7714e6a6c538cc0b397b580f943
Ehan Haque <ehann.hoq@gmail.com>
2025-02-03 22:47:27 -0500

    init

---
+++ convolution_neuron.py
@@ -0,0 +1,20 @@
+import numpy as np
+class ConvolutionNeuron:
+    kernel: np.ndarray
+    stride: int
+
+    activation: np.ndarray
+
+    def __init__(self, kernel: np.ndarray, stride: int = 1):
+        self.kernel = kernel
+        self.stride = stride
+
+    def convolve2d(self, input: np.ndarray):
+        output_rows: int = len(input) - len(self.kernel) + 1
+        output_cols: int = len(input[0]) - len(self.kernel[0]) + 1
+        activation = np.zeros((output_rows, output_cols), dtype=float)
+
+        for x in range(0, output_rows, self.stride):
+            for y in range(0, output_cols, self.stride):
+                window = np.array([ input[i][y:y + len(self.kernel[0])] for i in range(x, x + len(self.kernel)) ])
+                activation[x, y] = np.sum(window * self.kernel)
---
+++ train.py
@@ -0,0 +1,68 @@
+import numpy as np
+import matplotlib.image as mpimg
+import os
+import json
+
+import convolution_neuron as cn
+
+def load_data(path: str, grayscale: bool = True):
+    images = []
+
+    for filename in os.listdir(path):
+        if filename.lower().endswith(('.jpg', '.png', '.jpeg')):
+            image = mpimg.imread(os.path.join(path, filename))
+            if grayscale and len(image.shape) == 3:
+                image = np.dot(image[...,:3], [0.299, 0.587, 0.114])
+          
+            images.append(image)
+
+    return np.array(images)
+
+def save_kernels(nodes: np.ndarray):
+    kernels = [ node.kernel.tolist() for node in nodes ]
+    with open('kernels.json', "w") as f:
+        json.dump(kernels, f)
+
+def load_kernels() -> np.ndarray:
+    with open('kernels.json', "r") as f:
+        return np.array(json.load(f))
+    
+def leaky_relu(input: np.array, alpha: float = 0.1) -> np.array:
+    return np.where(input > 0, input, alpha * input)
+
+def max_pooling(input: np.array, size: int = 2, stride: int = 2) -> np.array:
+
+    output_rows: int = (len(input) - size) // stride + 1
+    output_cols: int = (len(input[0]) - size) // stride + 1
+
+    output = np.zeros((output_rows, output_cols))
+
+    for x in range(output_rows):
+        for y in range(output_cols):
+            window = input[x * stride:x * stride + size, y * stride:y * stride + size]
+            output[x, y] = np.max(window)
+        
+    return output
+    
+convolution_nodes = np.array([])
+
+if __name__ == '__main__':
+    # images = load_data('training_data', grayscale=False)
+
+    if os.path.exists('kernels.json'):
+        kernels = load_kernels()
+        convolution_nodes = np.array([cn.ConvolutionNeuron(kernels[i]) for i in range(len(kernels))])
+    else:
+        file = open('kernels.json', 'w')
+        file.close()
+
+        num_convolution_nodes = 2
+
+        for i in range(num_convolution_nodes):
+            kernel = np.random.randn(3, 3)
+            convolution_nodes = np.append(convolution_nodes, cn.ConvolutionNeuron(kernel))
+
+        save_kernels(convolution_nodes)
+
+    current_kernels = [ node.kernel.tolist() for node in convolution_nodes ]
+    print(current_kernels)
