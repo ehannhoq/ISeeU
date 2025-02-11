f7fa53faa1012192b6dc5eb1da15a284029bffaf
Ehan Haque <ehann.hoq@gmail.com>
2025-02-11 12:13:56 -0500

    debug messages

--- src/train.py
+++ src/train.py
@@ -2,11 +2,8 @@ import numpy as np
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import os
-import json
 import algorithms
 
-import convolution_neuron
-import neuron
 import model
 
 def load_images(path: str, grayscale: bool = True) -> np.ndarray:
@@ -44,9 +41,12 @@ if __name__ == '__main__':
                                     ((image_width - sample_cn_one.kernel.shape[0]) // sample_cn_one.stride),
                                     ((image_height - sample_cn_one.kernel.shape[1]) // sample_cn_one.stride)
                                     ), dtype=float)
+        algorithms.debug_message("Initialized first convolution layer output with shape: " + str(layer_one_output.shape), debug)
         for i, cn in enumerate(model.model["cn_layer_one"]):
             cn.convolve2d(image, nonlinear=True, pooling=True)
             layer_one_output[i] = cn.activation
+            algorithms.debug_message(f"Convolved Image {i} out of {len(model.model['cn_layer_one'])}", debug)
+        algorithms.debug_message("First layer convolution successful.", debug)
 
         # Second Convolution Layer (ouputs a 3D tensor)
         sample_cn_two = model.model["cn_layer_two"][0]
@@ -54,12 +54,16 @@ if __name__ == '__main__':
                                     ((layer_one_output.shape[1] - sample_cn_two.kernel.shape[0]) // sample_cn_two.stride), 
                                     ((layer_one_output.shape[2] - sample_cn_two.kernel.shape[1]) // sample_cn_two.stride),                                    
                                     ), dtype=float)
+        algorithms.debug_message("Initialized second convolution layer output with shape: " + str(layer_two_output.shape), debug)
         for i, cn in enumerate(model.model["cn_layer_two"]):
             cn.convolve3d(layer_one_output, nonlinear=True, pooling=True)
             layer_two_output[i] = cn.activation
+            algorithms.debug_message(f"Convolved Image {i} out of {len(model.model['cn_layer_two'])}", debug)
+        algorithms.debug_message("Second layer convolution successful.", debug)
 
         # Flattening output for fully connected layer.
         flattened_output = layer_two_output.flatten()
+        algorithms.debug_message(f"Flattened output to shape: {flattened_output.shape}", debug)
 
 
         # code that connects the convolved activation matricies to the fully connected layer
