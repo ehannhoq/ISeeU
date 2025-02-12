44b39afe362f4bf5c91ec6db54533326b8200eda
Ehan Haque <ehann.hoq@gmail.com>
2025-02-12 01:57:47 -0500

    implemented adaptive pooling method

--- src/algorithms.py
+++ src/algorithms.py
@@ -7,13 +7,12 @@ def leaky_relu(input: np.ndarray, alpha: float = 0.1) -> np.array:
 
 
 def max_pooling(input: np.ndarray, size: int = 2, stride: int = 2) -> np.array:
-
     assert len(input.shape) == 2, "Input must be 2D"
 
     input_height, input_width = input.shape
 
-    output_width: int = (input_width - size) // stride + 1
-    output_height: int = (input_height - size) // stride + 1
+    output_width = (input_width - size) // stride + 1
+    output_height = (input_height - size) // stride + 1
 
     output = np.zeros((output_height, output_width))
 
@@ -22,6 +21,26 @@ def max_pooling(input: np.ndarray, size: int = 2, stride: int = 2) -> np.array:
             window = input[ y * stride:y * stride + size, x * stride:x * stride + size ]
             output[y, x] = np.max(window)
         
+    return output
+
+def adaptive_pooling(input: np.ndarray, target_size: tuple) -> np.ndarray:
+    assert len(input.shape) == 2, "Input must be 2D"
+    
+    input_height, input_width = input.shape
+    output_height, output_width = target_size
+
+    stride_width = input_width // output_width
+    stride_height = input_height // output_height
+    size_width = stride_width
+    size_height = stride_height
+    
+    output = np.zeros(target_size, dtype=float)
+
+    for x in range(output_width):
+        for y in range(output_height):
+            window = input[y * stride_height:y * stride_height + size_height, x * stride_width:x * stride_width + size_width]
+            output[y, x] = np.max(window)
+    
     return output
 
 def resize_image(input: np.ndarray) -> np.ndarray:
