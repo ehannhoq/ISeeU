7381ef8de809835c23721da301e322b13c389981
Ehan Haque <ehann.hoq@gmail.com>
2025-02-17 01:23:33 -0500

    Revert to bbc1e1752a24e45f3a9fd5dc8feb261360ae5b8a

--- src/algorithms.py
+++ src/algorithms.py
@@ -1,12 +1,8 @@
 import numpy as np
 import cv2 as cv
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 def convolve2d(input: np.ndarray, kernel: np.ndarray) -> np.ndarray:
     assert len(kernel.shape) == len(input.shape), "Kernel and input dimension mismatch"
->>>>>>> parent of 0faf934 (training set now takes in rgb images)
 
     input_height, input_width = input.shape
     kernel_height, kernel_width = kernel.shape
@@ -62,8 +58,6 @@ def convolve_gradient(error_gradient: np.ndarray, kernel: np.ndarray) -> np.ndar
 
     return output
 
-=======
->>>>>>> parent of bbc1e17 (switched from oop to matricies)
 
 def leaky_relu(input, alpha: float = 0.1):
     return np.where(input > 0, input, alpha * input)
@@ -86,50 +80,23 @@ def max_pooling(input: np.ndarray, size: int = 2, stride: int = 2) -> np.array:
         
     return output
 
-def adaptive_pooling(input: np.ndarray, target_size: tuple) -> np.ndarray:
-    assert len(input.shape) == 2, "Input must be 2D"
-    
-    input_height, input_width = input.shape
-    output_height, output_width = target_size
-
-    stride_width = input_width // output_width
-    stride_height = input_height // output_height
-    size_width = stride_width
-    size_height = stride_height
-    
-    output = np.zeros(target_size, dtype=float)
-
-    for y in range(output_height):
-        for x in range(output_width):
-            window = input[y * stride_height:y * stride_height + size_height, x * stride_width:x * stride_width + size_width]
-            output[y, x] = np.max(window)
-    
-    return output
 
 def mean_squared_error_gradient(predicted: np.ndarray, expected: np.ndarray) -> np.ndarray:
     return 2 * (predicted - expected)
 
+
 def leaky_relu_gradient(input, alpha: float = 0.1):
     return np.where(input > 0, 1, alpha)    
 
-<<<<<<< HEAD
 
 def resize_image(image: np.ndarray, target_size: tuple) -> np.ndarray:
-<<<<<<< HEAD
-    current_height, current_width, current_channels = image.shape
-=======
-def resize_image(input: np.ndarray) -> np.ndarray:
-    current_width = np.size(input, 0)
-    current_height = np.size(input, 1)
->>>>>>> parent of bbc1e17 (switched from oop to matricies)
-=======
     current_height, current_width = image.shape
->>>>>>> parent of 0faf934 (training set now takes in rgb images)
 
     if current_width > current_height:
-        return cv.resize(input, (300, int(300 * current_height / current_width)))
+        aspect_ratio = current_width / current_height
+        new_width = target_size[1]
+        new_height = int(aspect_ratio * new_width)
     else:
-<<<<<<< HEAD
         aspect_ratio = current_height / current_width
         new_height = target_size[0]
         new_width = int(aspect_ratio * new_height)
@@ -139,6 +106,3 @@ def resize_image(input: np.ndarray) -> np.ndarray:
     output = np.zeros(target_size, dtype=image.dtype)
     output[ 0:resized_image.shape[0], 0:resized_image.shape[1] ] = resize_image
     return output
-=======
-        return cv.resize(input, (int(300 * current_width / current_height), 300))
->>>>>>> parent of bbc1e17 (switched from oop to matricies)
--- src/train.py
+++ src/train.py
@@ -1,49 +1,27 @@
 import numpy as np
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 import os
 import algorithms
 
 import model
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-def load_images(path: str) -> np.ndarray:
-=======
-def load_images(path: str, grayscale: bool = True) -> np.array:
->>>>>>> parent of bbc1e17 (switched from oop to matricies)
-=======
 def load_images(path: str, grayscale: bool = True) -> np.ndarray:
->>>>>>> parent of 0faf934 (training set now takes in rgb images)
     image_data = []
 
     for filename in os.listdir(path):
         if filename.lower().endswith(('.jpg', '.png', '.jpeg')):
             image = mpimg.imread(os.path.join(path, filename))
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
             if grayscale and len(image.shape) == 3:
                 image = np.dot(image[...,:3], [0.299, 0.587, 0.114])
 
->>>>>>> parent of 0faf934 (training set now takes in rgb images)
             aspect_ratio = image.shape[1] / image.shape[0]
             image = algorithms.resize_image(image=image, target_size=model.image_size)
 
 
             image_data.append(np.array([string_to_numbers(filename.split(".")[0])]), image, aspect_ratio)
-=======
-            if grayscale and len(image.shape) == 3:
-                image = np.dot(image[...,:3], [0.299, 0.587, 0.114])
-
-            # For training and testing, we're going to downscale every image from A x B -> C x D, with either C or D
-            # being 300 pixels, depending on whether A or B is greater. (ie. 600 x 400 -> 300 x 200, or 400 x 600 -> 200 x 300)
-            image = algorithms.resize_image(image)
-
-
-            image_data.append(np.array([string_to_numbers(filename.split(".")[0])]), image)
->>>>>>> parent of bbc1e17 (switched from oop to matricies)
 
     return np.array(image_data)
 
@@ -56,29 +34,18 @@ if __name__ == '__main__':
 
     model.load_model()
     
+
     # Training
     for data in images:
+
 
         # Preprosessing
         expected = data[0]
         image = data[1]
+        aspect_ratio = data[2]
         learning_rate = 0.005
 
-        # First Convolution Layer (outputs a 3D tensor)
-        image_width = np.size(image, 0) 
-        image_height = np.size(image, 1)
-        sample_convolution_neuron_l1 = model.model["cn_layer_one"][0]
-        cn_l1_output = np.zeros((len(model.model["cn_layer_one"]), 
-                                    ((image_width - sample_convolution_neuron_l1.kernel.shape[0]) // sample_convolution_neuron_l1.stride),
-                                    ((image_height - sample_convolution_neuron_l1.kernel.shape[1]) // sample_convolution_neuron_l1.stride)
-                                    ), 
-                                    dtype=float
-                                )
-        for i, convolution_neuron_l1 in enumerate(model.model["cn_layer_one"]):
-            convolution_neuron_l1.convolve2d(image)
-            cn_l1_output[i] = convolution_neuron_l1.activation
 
-<<<<<<< HEAD
         # First Convolution Layer
         cn1_activation = np.zeros((
             model.cn1_neurons,
@@ -88,25 +55,8 @@ if __name__ == '__main__':
         for i in range(model.cn1_neurons):
             cn1_activation[i] = algorithms.convolve2d(image, model.model["w_cn1"][i]) + model.model["b_cn1"][i]
         cn1_activation = algorithms.max_pooling(cn1_activation)
-=======
-        # Applying non-linearity and pooling.
-        cn_l1_output = algorithms.leaky_relu(cn_l1_output)
-        cn_l1_output = algorithms.max_pooling(cn_l1_output)
->>>>>>> parent of bbc1e17 (switched from oop to matricies)
 
-        # Second Convolution Layer (ouputs a 3D tensor)
-        sample_convolution_neuron_l2 = model.model["cn_layer_two"][0]
-        cn_l2_output = np.zeros((len(model.model["cn_layer_two"]),
-                                    ((cn_l1_output.shape[1] - sample_convolution_neuron_l2.kernel.shape[0]) // sample_convolution_neuron_l2.stride), 
-                                    ((cn_l1_output.shape[2] - sample_convolution_neuron_l2.kernel.shape[1]) // sample_convolution_neuron_l2.stride),                                    
-                                    ), 
-                                    dtype=float
-                                )
-        for i, convolution_neuron_l2 in enumerate(model.model["cn_layer_two"]):
-            convolution_neuron_l2.convolve3d(cn_l1_output)
-            cn_l2_output[i] = convolution_neuron_l2.activation
 
-<<<<<<< HEAD
         # Second Convolution Layer
         cn2_activation = np.zeros((
             model.cn2_neurons,
@@ -116,45 +66,18 @@ if __name__ == '__main__':
         for i in range(model.cn2_neurons):
             cn2_activation[i] = algorithms.convolve3d(algorithms.leaky_relu(cn1_activation), model.model["w_cn2"][i]) + model.model["b_cn2"][i]
         cn2_activation = algorithms.max_pooling(cn2_activation)
-=======
-        # Applying non-linearity and pooling.
-        cn_l2_output = algorithms.leaky_relu(cn_l2_output)
-        cn_l2_output = algorithms.max_pooling(cn_l2_output)
->>>>>>> parent of bbc1e17 (switched from oop to matricies)
 
-        # Enforce adaptive pooling to ensure all outputs are the same size, regardless of image input.
-        sample_fc_neuron_l1 = model.model["fc_layer_one"][0]
-        target_size = (sample_fc_neuron_l1.weights.shape[0], sample_fc_neuron_l1.weights.shape[0])
-        cn_l2_output = algorithms.adaptive_pooling(cn_l2_output, target_size=target_size)
 
-        # Flattening adaptively pooled output for fully connected layer.
-        flattened_output = cn_l2_output.flatten()
+        # Flattened output for fully connected layer
+        flattened_cn_output = algorithms.leaky_relu(cn2_activation).flatten()
 
-        # First Fully Connected Layer (outputs a 1D array)
-        fc_l1_output = np.zeros((len(model.model["fc_layer_one"])), dtype=float)
-        for i, fc_neuron_l1 in enumerate(model.model["fc_layer_one"]):
-            fc_neuron_l1.calculate_activation(flattened_output)
-            fc_l1_output[i] = fc_neuron_l1.activation
 
-        # Applying non-linearity
-        fc_l1_output = algorithms.leaky_relu(fc_l1_output)
-
-        # Second Fully Connected Layer (outputs a 1D array)
-        fc_l2_output = np.zeros((len(model.model["fc_layer_two"])),dtype=float)
-        for i, fc_neuron_l2 in enumerate(model.model["fc_layer_two"]):
-            fc_neuron_l2.calculate_activation(fc_l1_output)
-            fc_l2_output[i] = fc_neuron_l2.activation
-
-        # Applying non-linearity
-        fc_l2_output = algorithms.leaky_relu(fc_l2_output)
+        # First Fully Connected Layer
+        fc1_activation = np.zeros((model.fc1_neurons))
+        for i in range(model.fc1_neurons):
+            fc1_activation[i] = np.dot(flattened_cn_output, model.model["w_fc1"][i]) + model.model["b_fc1"][i]
         
-        # Output Layer
-        output = np.zeros((len(model.model["output_layer"])), dtype=float)
-        for i, output_neuron in enumerate(model.model["output_layer"]):
-            output_neuron.calculate_activation(fc_l2_output)
-            output[i] = output_neuron.activation
 
-<<<<<<< HEAD
         # Second Fully Connected Layer
         fc2_activation = np.zeros((model.fc2_neurons))
         for i in range(model.fc2_neurons):
@@ -230,63 +153,11 @@ if __name__ == '__main__':
 
 
 
-=======
-        predicted_x = output[0].activation
-        predicted_y = output[1].activation
-        predicted_width = output[2].activation
-        predicted_height = output[3].activation
->>>>>>> parent of bbc1e17 (switched from oop to matricies)
 
-        # Calculating error gradient for each layer.
-        output_error_gradient = algorithms.mean_squared_error_gradient(
-            np.array(predicted_x, predicted_y, predicted_width, predicted_height), 
-            expected
-        )
 
-        fc2_error_gradient = np.zeros((len(model.model["fc_layer_two"])), dtype=float)
-        for i, fc_l2_neuron in enumerate(model.model["fc_layer_two"]):
-            activation_derivative = algorithms.leaky_relu_gradient(fc_l2_neuron.activation)
-            fc2_error_gradient[i] = np.dot(output_error_gradient - fc_l2_neuron.weights) * activation_derivative
 
         
-        fc1_error_gradient = np.zeros((len(model.model["fc_layer_one"])), dtype=float)
-        for i, fc_l1_neuron in enumerate(model.model["fc_layer_one"]):
-            activation_derivative = algorithms.leaky_relu_gradient(fc_l1_neuron.activation)
-            fc1_error_gradient[i] = np.dot(fc2_error_gradient - fc_l1_neuron.weights) * activation_derivative
-
-
-        fc1_error_gradient_3d = fc1_error_gradient.reshape(cn_l2_output.shape)
-        cn2_error_gradient = np.zeros_like(cn_l2_output)
-        for i, cn_l2_neuron in enumerate(model.model["cn_layer_two"]):
-            activation_derivative = algorithms.leaky_relu_gradient(cn_l2_neuron.activation)
-            cn2_error_gradient[i] = cn_l2_neuron.convolve_gradient(fc1_error_gradient[i]) * activation_derivative
-
-        cn1_error_gradient = np.zeros_like(cn_l1_output)
-        for i, cn_l1_neuron in enumerate(model.model["cn_layer_one"]):
-            activation_derivative = algorithms.leaky_relu_gradient(cn_l1_neuron.activation)
-            cn1_error_gradient[i] = cn_l1_neuron.convolve_gradient(cn2_error_gradient) * activation_derivative
-
-        # Adjusting weights and biases
-        for i, output_neuron in enumerate(model.model["output_layer"]):
-            output_neuron.weights -= learning_rate * output_error_gradient[i] * np.array(fc_l2_output)
-            output_neuron.bias -= learning_rate * output_error_gradient[i]
-
-        for i, fc_l2_neuron in enumerate(model.model["fc_layer_two"]):
-            fc_l2_neuron.weights -= learning_rate * fc2_error_gradient[i] * fc_l2_neuron.activation
-            fc_l2_neuron.bias -= learning_rate * fc2_error_gradient[i]
-
-        for i, fc_l1_neuron in enumerate(model.model["fc_layer_one"]):
-            fc_l1_neuron.weights -= learning_rate * fc1_error_gradient[i] * fc_l1_neuron.activation
-            fc_l1_neuron.bias -= learning_rate * fc1_error_gradient[i]
-
-        for i, cn_l2_neuron in enumerate(model.model["cn_layer_two"]):
-            cn_l2_neuron.kernel -= learning_rate * cn2_error_gradient[i] * cn_l2_neuron.activation
-            cn_l2_neuron.bias -= learning_rate * cn2_error_gradient[i]
-
-        for i, cn_l1_neuron in enumerate(model.model["cn_layer_one"]):
-            cn_l1_neuron.kernel -= learning_rate * cn1_error_gradient[i] * cn_l1_neuron.activation
-            cn_l1_neuron.bias -= learning_rate * cn1_error_gradient[i]
-
+        
         
 
             
