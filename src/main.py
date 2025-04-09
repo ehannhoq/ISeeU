a0167109239755208e57fd4fc26d0b32ebf2aaa5
Ehan Haque <ehann.hoq@gmail.com>
2025-04-09 16:00:29 -0400

    added args for terminal/command prompt

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
+import argparse
+
 import iseeu
 import time
 from algorithms import load_wider_data_set
 
-
+parser = argparse.ArgumentParser()
+parser.add_argument("--batch_size", type=int, default=4)
+parser.add_argument("--epochs", type=int, default=1)
+args = parser.parse_args()
 if __name__ == '__main__':
     model = iseeu.Model(
         learning_rate=0.001,
@@ -11,17 +16,18 @@ if __name__ == '__main__':
         show_debug=True
         )
     
-    epoch = 0
+    max_epochs = args.epochs
+    epoch = args.epochs
     i = 0
-    while i != -1 and epoch < 1000:
-        print(f"Epoch: {epoch}")
+    while i != -1 and epoch > 0:
+        print(f"Epoch: {max_epochs - epoch + 1}/{max_epochs}")
         start_time = time.time()
 
         images, expected, i = load_wider_data_set(
             imageset_master_path='training_data',
             annotation_file_path='training_data/image_info.txt',
             target_size=(500, 500),
-            batch_size=1,
+            batch_size=args.batch_size,
             max_faces=10,
             start_index=i
         )
@@ -32,5 +38,5 @@ if __name__ == '__main__':
         )
 
         end_time = time.time()
-        print(f"Time elapsed for epoch {epoch + 1}: {(end_time - start_time):.2f} seconds")
-        epoch += 1
+        print(f"Time elapsed for epoch {max_epochs - epoch + 1}: {(end_time - start_time):.2f} seconds")
+        epoch -= 1
